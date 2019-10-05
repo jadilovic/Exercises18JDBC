cc1d3475959d5b34a5baece89d1a4db848a6c962
jadilovic <adilovic79@yahoo.com>
2019-10-05 18:10:16 +0200

    connect sql

--- ConnectToMySQL.java
+++ ConnectToMySQL.java
@@ -1,10 +1,29 @@
 package Vjezbe;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectToMySQL {
+	
+	private static final String USER_NAME = "root";
+	private static final String PASSWORD = "password";
+	private static final String CONN_STRING = "jdbc:mysql://localhost/students3";
+	
+	public static void main(String[] args) throws SQLException {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection(CONN_STRING, USER_NAME, PASSWORD);
+			System.out.println("Connection is successful");
+			
+		} catch (SQLException e){
+			System.err.println(e);
+		} finally {
+			if(connection != null)
+				connection.close();
+		}
 	}
 
 }
