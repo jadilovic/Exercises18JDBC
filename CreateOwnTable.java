8c717854bf67de3b060d89b82fce25947efd53ee
jadilovic <adilovic79@yahoo.com>
2019-10-07 09:07:08 +0200

    create table

--- CreateOwnTable.java
+++ CreateOwnTable.java
@@ -1,10 +1,32 @@
 package Vjezbe;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateOwnTable {
+	
+	private static final String USER_NAME = "root";
+	private static final String PASSWORD = "password";
+	private static final String CONN_DB = "jdbc:mysql://localhost/students2";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String sqlQuery = "CREATE TABLE info ("
+				+ "id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, "
+				+ "name VARCHAR(70), "
+				+ "lastName VARCHAR(70), "
+				+ "age DATE, "
+				+ "email VARCHAR(70)"
+				+ ");";
+		
+		try(Connection connection = DriverManager.getConnection(CONN_DB, USER_NAME, PASSWORD)){
+			Statement statement = connection.createStatement();
+			System.out.println(statement.executeUpdate(sqlQuery));
+			System.out.println("Table was created");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
